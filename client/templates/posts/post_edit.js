bd792b99888d0dd9c57e29d13eb86c01910d83a6
Gilberto Donato <gilbertodonato@Gilbertos-MacBook-Pro.local>
2016-04-26 08:26:16 +0800

    9-5.1

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -1,3 +1,23 @@
+Template.postEdit.onCreated(function(){
+  Session.set('postEditErrors',{});
+});
+
+
+Template.postEdit.helpers({
+  errorMessage: function(field){
+    return Session.get('postEditErrors')[field];
+
+  },
+  errorClass: function(field){
+    return !!Session.get('postEditErrors')[field]?'has-error':'';
+
+  }
+
+});
+
+
+
+
 Template.postEdit.events({
     'submit form':function(e){
         e.preventDefault();
@@ -8,6 +28,11 @@ Template.postEdit.events({
             title: $(e.target).find('[name=title]').val()
         }
 
+        var errors = validatePost(postProperties);
+
+        if (errors.title || errors.url){
+          return Session.set('postEditErrors',errors);
+        }
         Posts.update(currentPostId, {$set: postProperties}, function(error){
             if(error){
                 throwError(error.reason);
