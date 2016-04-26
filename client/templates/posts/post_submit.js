f2674b50f94684453b720af6024288c28a4040f4
Gilberto Donato <gilbertodonato@Gilbertos-MacBook-Pro.local>
2016-04-26 08:00:48 +0800

    9-4

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -19,6 +19,13 @@ Template.postSubmit.events({
                 url:$(e.target).find('[name=url]').val(),
                 title:$(e.target).find('[name=title]').val()
         };
+
+        var errors = validatePost(post);
+
+        if (errors.title || errors.url){
+          return Session.set('postSubmitErrors',errors);
+        }
+
         Meteor.call('postInsert', post, function(error, result){
             if (error)
                 return alert(error.reason);
