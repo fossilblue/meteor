c3174bd4eb6d1cde625bfdc045e41013dd92096e
Gilbert Donato <fossilblue@gmail.com>
2016-05-07 21:43:36 +0800

    12-5: spinner

--- server/publications.js
+++ server/publications.js
@@ -1,17 +1,17 @@
 Meteor.publish('posts', function(options){
-    check(options,{
-      sort: Object,
-      limit:Number
-    });
+      return Posts.find({},options);
+});
 
-    return Posts.find({},options);
-    });
+Meteor.publish('singlePost', function(options){
+  check(id,String);
+  return Posts.find(id);
+});
 
 
-    Meteor.publish('comments', function(postId) {
-      check(postId, String);
-      return Comments.find({postId: postId});
-    });
+Meteor.publish('comments', function(postId) {
+  check(postId, String);
+  return Comments.find({postId: postId});
+});
 
 Meteor.publish('notifications', function(){
   return Notifications.find({userId:this.userId,read:false});
